90f0fc5831d6aeee683b600f8f45175e8d7e8672
marvikomo <marvikomo@gmail.com>
2023-01-05 14:40:20 +0100

    added validation

--- backend/app/Http/Controllers/ItemController.php
+++ backend/app/Http/Controllers/ItemController.php
@@ -7,79 +7,149 @@ use Illuminate\Http\Request;
 use App\Models\TodoItem;
 use App\Http\Resources\ItemResource;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
 
-class ItemController extends Controller{
-    
-    public function createItem(Request $request){
-        
-        $item = TodoItem::create([
-          'name' => $request->name,
-          'description' => $request->description,
-          'photo' => $request->photo,
-          'amount' => $request->amount
-        ]);
-        //return new ItemResource($item->get());
-       // return (new ItemResource($item))->response()->setStatusCode(Response::HTTP_CREATED);
-       return response()->json($item->toArray(), Response::HTTP_CREATED);
-    }
+class ItemController extends Controller
+{
+    public function createItem(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required',
+                'description' => 'required',
+                'photo' => 'required',
+                'amount' => 'required',
+            ]);
+            if ($validator->fails()) {
+                // Validation failed, return error response
+                return response()->json(
+                    [
+                        'error' => $validator->errors(),
+                    ],
+                    400
+                );
+            }
+            $item = TodoItem::create([
+                'name' => $request->name,
+                'description' => $request->description,
+                'photo' => $request->photo,
+                'amount' => $request->amount,
+            ]);
 
-    public function retrieveItems(Request $request) {
-        $items = TodoItem::all();
-        return response()->json($items->toArray(), 200);
-    }
-
-    public function retrieveSingleItem(Request $request, $id) {
-       $item = TodoItem::find($id);
-       return response()->json($item->toArray(), 200);
-    }
-
-    public function updateItem(Request $request, $id) {
-        try{
-        // $request->validate([
-        //     'name'=>'required',
-        //     'description'=>'required',
-        //     'amount'=>'required',
-        //     'photo' => 'required'
-        // ]); 
-        
-        $todo = TodoItem::find($id);
-        // Getting values from the blade template form
-        $todo->name =  $request->name;
-        $todo->description = $request->description;
-        $todo->amount = $request->amount;
-        $todo->photo = $request->photo;
-        $todo->save();
-
-        return response()->json(['success' => true, 'message' => 'List has been updated.'], Response::HTTP_OK);
-    }catch(\Exception $e){
-
-    }
-
-    }
-    
-    public function deleteItem(Request $request, $id) {
-        try{
-     $item = TodoItem::findOrFail($id);
-     $item->delete();
-     return response()->json([], Response::HTTP_OK);
-        }catch(\Exception $e){
-            return response()->json([
-                'error' => $e->getMessage(),
-            ], 500);
+            return response()->json($item->toArray(), Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'error' => $e->getMessage(),
+                ],
+                500
+            );
         }
     }
 
-    public function complete(Request $request, $id) {
-        try{
-            $item = TodoItem::findOrFail($id);
-            $item->status = "COMPLETED";
-            $item->save();
-            return response()->json(['success' => true, 'message' => 'List has been completed.'], Response::HTTP_OK);
+    public function retrieveItems(Request $request)
+    {
+        try {
+            $items = TodoItem::all();
+            return response()->json($items->toArray(), 200);
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'error' => $e->getMessage(),
+                ],
+                500
+            );
+        }
+    }
 
-        }catch(\Exception $e){
-            return response()->json([
-                'error' => $e->getMessage(),
-            ], 500);
+    public function retrieveSingleItem(Request $request, $id)
+    {
+        try {
+            $item = TodoItem::find($id);
+            return response()->json($item->toArray(), 200);
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'error' => $e->getMessage(),
+                ],
+                500
+            );
+        }
+    }
+
+    public function updateItem(Request $request, $id)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required',
+                'description' => 'required',
+                'photo' => 'required',
+                'amount' => 'required',
+            ]);
+            if ($validator->fails()) {
+                // Validation failed, return error response
+                return response()->json(
+                    [
+                        'error' => $validator->errors(),
+                    ],
+                    400
+                );
+            }
+
+            $todo = TodoItem::find($id);
+            $todo->name = $request->name;
+            $todo->description = $request->description;
+            $todo->amount = $request->amount;
+            $todo->photo = $request->photo;
+            $todo->save();
+
+            return response()->json(
+                ['success' => true, 'message' => 'List has been updated.'],
+                Response::HTTP_OK
+            );
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'error' => $e->getMessage(),
+                ],
+                500
+            );
+        }
+    }
+
+    public function deleteItem(Request $request, $id)
+    {
+        try {
+            $item = TodoItem::findOrFail($id);
+            $item->delete();
+            return response()->json([], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'error' => $e->getMessage(),
+                ],
+                500
+            );
+        }
+    }
+
+    public function complete(Request $request, $id)
+    {
+        try {
+            $item = TodoItem::findOrFail($id);
+            $item->status = 'COMPLETED';
+            $item->save();
+            return response()->json(
+                ['success' => true, 'message' => 'List has been completed.'],
+                Response::HTTP_OK
+            );
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'error' => $e->getMessage(),
+                ],
+                500
+            );
         }
     }
 }
